a4f201a49836883ceb199393a44d17354d8e490e
Natan-Mesele <huluale12@gmail.com>
2023-12-22 16:52:26 +0300

    movie trailer

--- src/components/Requests.js
+++ src/components/Requests.js
@@ -1,7 +1,7 @@
 const APIKEY = "8b3c8eb7ab57e1f1b000d4886802f6b3";
 
 const requests = {
-    fetchTrending: `/trending/all/week?api_key=${APIKEY}&language=en-US`,
+  fetchTrending: `/trending/all/week?api_key=${APIKEY}&language=en-US`,
   fetchNetflixOriginals: `/discover/tv?api_key=${APIKEY}&with_networks=213`,
   fetchTopRated: `/movie/top_rated?api_key=${APIKEY}&language=en-US`,
   fetchTopActionMovies: `/discover/movie?api_key=${APIKEY}&with_geners=28`,
--- src/components/Row.js
+++ src/components/Row.js
@@ -1,12 +1,14 @@
 import React, { useState, useEffect } from "react";
 import axios from "../components/axios";
 import "./Row.css";
+import Youtube from "react-youtube";
+import movieTrailer from "movie-trailer";
 
 const base_url = "https://image.tmdb.org/t/p/original/";
 
 function Row({ title, fetchUrl, isLargeRow = false }) {
   const [movies, setMovies] = useState([]);
-
+  const [trailerUrl, setTrailerUrl] = useState("");
   
   useEffect(() => {
     async function fetchData() {
@@ -17,7 +19,25 @@ function Row({ title, fetchUrl, isLargeRow = false }) {
     fetchData();
   }, [fetchUrl]);
 
-  console.log(movies);
+  const opts = {
+    height: "300",
+    width: "100%",
+    playerVars: {
+      autoplay: 1,
+    },
+  };
+
+  const handleClick = (movie) => {
+    if (trailerUrl){
+      setTrailerUrl('');
+    } else {
+      movieTrailer(movie?.name || "")
+      .then(url => {
+        const urlParams = new URLSearchParams(new URL(url).search);
+        setTrailerUrl(urlParams.get("v"));
+      }).catch((error) => console.log(error));
+    }
+  }
 
   return (
     <div className="row">
@@ -28,6 +48,7 @@ function Row({ title, fetchUrl, isLargeRow = false }) {
         (!isLargeRow && movie.backdrop_path)) && (
           <img
             key={movie.id}
+            onClick={() => handleClick(movie)}
             className={`row_poster ${isLargeRow && "row_posterLarge"}`}
             src={`${base_url}${
               isLargeRow ? movie.poster_path : movie.backdrop_path
@@ -36,6 +57,7 @@ function Row({ title, fetchUrl, isLargeRow = false }) {
           />
         ))}
       </div>
+      {trailerUrl && <Youtube videoId={trailerUrl} opts={opts}/>}
     </div>
   );
 }
